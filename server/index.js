b7b0e020a456f88cda5b2ed52a4db0f61a42f29d
bigyam <aruan.careers@gmail.com>
2019-06-02 01:08:33 -0400

    applied changes for deployment

--- server/index.js
+++ server/index.js
@@ -6,7 +6,8 @@ const bodyParser = require('body-parser');
 var db = require('./database');
 
 //set environment and port if none provided
-const ENV = process.env.NODE_ENV;
+//const ENV = process.env.NODE_ENV;
+const ENV = 'production';
 const PORT = process.env.PORT || 5000;
 
 //configure app to use body parser.  let us get data from POST
@@ -16,6 +17,13 @@ app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use('/api/todo', require('./api/todo'));
+
+if (ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use((req, res) => {
+		res.sendFile(path.join(__dirname, '../client/build/index.html'));
+	});
+}
 
 //start listen
 app.listen(PORT, () => {
